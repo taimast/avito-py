89180aaf34063fc254a6e7def5666721adcbc093
taimast <charlskenno@gmail.com>
2024-02-18 16:29:37 +0300

    minor fix

--- avito/schema/rating/methods.py
+++ avito/schema/rating/methods.py
@@ -3,5 +3,6 @@ from.models import RatingInfo
 
 
 class GetRatingsInfo(AvitoMethod[RatingInfo]):
+    __request_method__ = "GET"
     __returning__ = RatingInfo
     __api_method__ = "ratings/v1/info"
